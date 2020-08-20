4015310a452138b0a3c5714e0ae6cc96bfe5c4be
Nguyễn Đình Nhật <nguyendnhat@gmail.com>
2020-08-20 10:36:50 +0700

    Updates

---
+++ precache-manifest.78cc495b497c7161c0dd07a2940d15c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61573f8d5432f506183459e3be238553",
+    "url": "/creboo/index.html"
+  },
+  {
+    "revision": "ec56ea8122f3c05454ab",
+    "url": "/creboo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d810ae6119d91a5e5aa2",
+    "url": "/creboo/static/js/2.d34e600d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/creboo/static/js/2.d34e600d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec56ea8122f3c05454ab",
+    "url": "/creboo/static/js/main.03bbba97.chunk.js"
+  },
+  {
+    "revision": "989c05881aa6640c405c",
+    "url": "/creboo/static/js/runtime-main.78e47477.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/creboo/static/media/logo.5d5d9eef.svg"
+  }
+]);
